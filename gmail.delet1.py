a2eaf991c05f029541bc00606a3909790315a4e5
aleshn1 <122236649+aleshn1@users.noreply.github.com>
2023-02-03 16:05:57 -0300

    Create gmail.delet1.py

---
+++ gmail.delet1.py
@@ -0,0 +1,36 @@
+import webbrowser as wb
+import pyautogui as py
+from time import sleep 
+
+#pyautogui.PAUSE=2
+wb.open('www.google.com')
+sleep(4)
+py.click(x=806,y=101),
+sleep(6)
+for i in range(10):
+    py.click(x=281,y=255),
+    sleep(0.2)
+    py.click(x=283,y=295),
+    sleep(0.2)
+    py.click(x=285,y=338),
+    sleep(0.2)
+    py.click(x=284,y=378),
+    sleep(0.2)
+    py.click(x=280,y=418),
+    sleep(0.2)
+    py.click(x=282,y=457),
+    sleep(0.2)
+    py.click(x=279,y=493),
+    sleep(0.2)
+    py.click(x=283,y=534),
+    sleep(0.2)
+    py.click(x=281,y=579),
+    sleep(0.2)
+    py.click(x=282,y=614),
+    sleep(0.2)
+    py.click(x=284,y=658),
+    sleep(0.2)
+    py.click(x=429,y=159),
+    sleep(4)
+py.click(x=1002,y=11) 
+
